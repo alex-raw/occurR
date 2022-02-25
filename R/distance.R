4ae0301d71388557e8bd3d5d85e46574e86be88e
alex-raw <alexanderrauhutjs@gmail.com>
2022-02-25 22:55:22 +0100

    geometric correction made optional, moved dwg to minimal

--- R/distance.R
+++ R/distance.R
@@ -1,7 +1,11 @@
-dwg <- function(cpos, f, size) { # word growth dispersion
-    dd <- cpos - data.table::shift(cpos, fill = 0L) - (size / f)
-    mad <- sum(abs(dd)) / f
-    worst_mad <- (size - f + 1 - size / f) / (f / 2)
-    dwg <- mad / worst_mad
-    dwg / (2 * atan(worst_mad) / atan(mad))
+distance <- function(cpos, f, size, corr = TRUE) {
+  # TODO: only word growth dispersion for now
+  dd <- cpos - data.table::shift(cpos, fill = 0L) - (size / f)
+  mad <- sum(abs(dd)) / f
+  worst_mad <- (size - f + 1 - size / f) / (f / 2)
+  dwg <- mad / worst_mad
+  if (isTRUE(corr)) {
+    dwg <- dwg / (2 * atan(worst_mad) / atan(mad))
+  }
+  dwg
 }
--- R/minimal.R
+++ R/minimal.R
@@ -16,7 +16,7 @@
 #'
 #' @export
 ll <- function(f1, o11, n = sum(f1), f2 = sum(o11), one_sided = TRUE) {
-  o <- cbind(o11,                 o12 = f2 - o11,
+  o <- cbind(o11 = o11,           o12 = f2 - o11,
              o21 = f1 - o11,      o22 = n - f1 - f2 + o11)
   e <- cbind(e11 = f1 * f2,       e12 = (n - f1) * f2,
              e21 = f1 * (n - f2), e22 = (n - f1) * (n - f2)) / n
@@ -26,4 +26,31 @@ ll <- function(f1, o11, n = sum(f1), f2 = sum(o11), one_sided = TRUE) {
     ans[repulsed] <- -ans[repulsed]
   }
   ans
+}
+
+#' Word Growth Dispersion
+#'
+#' A distance-based dispersion measure
+#'
+#' @param cpos integer vector representing the indeces of a token
+#' @param f integer count of a token
+#' @param size integer corpus size
+#' @param corr logical whether or not to apply a geometric correction, see
+#' Details
+#'
+#' @return numeric
+#' @details TODO
+#' @examples
+#' dwg(c(41, 32, 41, 12), f = 4, size = 100)
+#'
+#' @export
+dwg <- function(cpos, f, size, corr = TRUE) { # word growth dispersion
+  dd <- cpos - data.table::shift(cpos, fill = 0L) - (size / f)
+  mad <- sum(abs(dd)) / f
+  worst_mad <- (size - f + 1 - size / f) / (f / 2)
+  dwg <- mad / worst_mad
+  if (isTRUE(corr)) {
+    dwg <- dwg / (2 * atan(worst_mad) / atan(mad))
+  }
+  dwg
 }
