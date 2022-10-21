b47173fdf7c64b93b3ae99fb7d82a88a8228211e
Alexander Rauhut <alexraw@mailbox.org>
2022-10-21 19:41:52 +0200

    feat(data): added sentence ids to brown

--- data-raw/get_brown.R
+++ data-raw/get_brown.R
@@ -7,7 +7,7 @@
 # everything but headings is indented with one leading "\t"
 # tokens consist of word/pos-tags
 # literal "/" are not escaped
-# "/" does not occur in tag, therefore last "/" in pair can be replaced with "\t"
+# "/" does not occur in tag, therefore last "/" in pair can be replaced with " "
 # -------
 
 # wrapper to partially apply arguments that ensure all characters are read as is
@@ -15,7 +15,7 @@
   function(...) {
     fun(...,
       na.strings = "",
-      quote = "\"",
+      quote = "",
       comment.char = "",
       allowEscapes = FALSE,
       strip.white = TRUE,
@@ -24,55 +24,56 @@
   }
 }
 
-.gsub_last_slash <- \(x) gsub("/([^/]*)$", " \\1", x)
-.scan <- \(...) .text_as_is(scan)(..., what = "", quiet = TRUE)
-.read_txt <- \(...) .text_as_is(utils::read.table)(..., stringsAsFactors = TRUE)
-.read_char <- \(.f) readChar(.f, file.info(.f)$size)
-
-
-parse_brown_file <- function(file, id, genre) {  # nolint
-  .scan(as.character(file)) |>  # split on whitespace, get vector
-    .gsub_last_slash() |>       # replace delimiter
-    .read_txt(text = _) |>      # split again, get data.frame
-    cbind(id, genre, cols = _)  # recycling here
-}
+.scan <- \(...) .text_as_is(scan)(..., sep = "\n", what = "", quiet = TRUE)
+.read_table <- \(...) .text_as_is(utils::read.table)(..., stringsAsFactors = TRUE)
 
 
 # process text files based on index and add genre and file ids as columns
-process_texts <- function(index_path, .fun) {
-  index <- .read_txt(index_path)
+process_texts <- function(index_path) {
+  index <- .read_table(index_path)
   files <- file.path(dirname(index_path), index[, 1])
-  Map(.fun, files, index[, 1], index[, 2], USE.NAMES = FALSE) |>
-    do.call(rbind, args = _)
+
+  .lines <- lapply(files, .scan)          # split on \n to get sentences
+  nlines <- lengths(.lines)
+
+  tokens <- strsplit(unlist(.lines), " ") # split on white to get tokens
+  ntokens <- lengths(tokens)
+
+  tokens <- unlist(tokens) |>
+    gsub("/([^/]*)$", " \\1", x = _) |>   # replace last "/" with white
+    .read_table(text = _)                 # split again on white, get word/pos
+
+  text_ids <- rep(index[, 1], nlines) |> rep(ntokens)
+  genre_ids <- rep(index[, 2], nlines) |> rep(ntokens)
+  sentence_ids <- seq_len(sum(nlines)) |> rep(ntokens)
+
+  cbind(genre_ids, text_ids, sentence_ids, tokens)
 }
 
-
-# download the Brown corpus and extract
-get_brown <- function(url, archive_path) {
-  utils::download.file(url, archive_path, quiet = TRUE)
+get_brown <- function(.url, archive_path) {
+  utils::download.file(.url, archive_path, quiet = TRUE)
   utils::unzip(archive_path, exdir = dirname(archive_path), junkpaths = TRUE)
 }
 
-
 main <- function() {
   temp_path <- tempdir()
-  at_temp <- \(path) file.path(temp_path, path)
+  in_temp <- \(path) file.path(temp_path, path)
 
   # data as linked on https://www.nltk.org/nltk_data/
-  url <- "https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/corpora/brown.zip"  # nolint
-  get_brown(url, archive_path = at_temp(basename(url)))
+  .url <- "https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/corpora/brown.zip"  # nolint
+  get_brown(.url, archive_path = in_temp(basename(.url)))
 
   structure(
-    process_texts(
-      index_path = at_temp("cats.txt"),
-      .fun = parse_brown_file
-    ),
-    names = c("id", "genre", "word", "pos"),
-    contents = .read_char(at_temp("CONTENTS")),
-    readme = .read_char(at_temp("README"))
+    process_texts(index_path = in_temp("cats.txt")),
+    names = c("genre", "text", "sentence", "word", "pos"),
+    contents = readLines(in_temp("CONTENTS")),
+    readme = readLines(in_temp("README"))
   )
 }
 
 brown <- main()
+
+write.table(brown, "brown.tsv",
+  sep = "\t", na = "", quote = FALSE, row.names = FALSE)
 
 usethis::use_data(brown, overwrite = TRUE)
