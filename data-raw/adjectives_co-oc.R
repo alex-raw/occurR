d190f113932688c037de05e6aa394df625d01bf1
Alexander Rauhut <alexraw@mailbox.org>
2022-10-26 12:10:49 +0200

    perf(data-raw): improved sentential co-occurrence script

--- data-raw/adjectives_co-oc.R
+++ data-raw/adjectives_co-oc.R
@@ -1,32 +1,32 @@
 # data set on sentential adjective co-occurrence, cf. Justeson & Katz 1991
 
+# find and count all combinations of 2 adjectives within a sentence
 count_pairs_per_sentence <- function(words, s_id) {
-  # find all combinations of 2 adjectives within a sentence
-  combos <- tapply(words, s_id, function(x) {
-    if (length(x) > 1)
-      unique(combn(x, 2, sort, simplify = FALSE)) # ab = ba and count only once
-  }) |>
-    unlist(recursive = FALSE) # get a list of pairs
-
+  combos <- fastmatch::ctapply(words, s_id, function(x) {
+    if (length(x) > 1) {
+      unique(combn(x, 2, sort.int, simplify = FALSE))
+    }
+  }) # get a list of pairs; sort so ab = ba and counted only once
   uniques <- unique(combos)
+
   data.frame(
-    matrix(unlist(uniques), ncol = 2, byrow = TRUE),
-    as.vector(table(match(combos, uniques)))
+    do.call(rbind, uniques),
+    as.vector(table(fastmatch::fmatch(combos, uniques)))
   )
 }
 
-get_adjectives <- function(corpus, tag) {
+get_pos <- function(corpus, tag) {
   res <- subset(corpus, grepl(tag, pos), c(word, sentence))
-  res$word <- as.factor(tolower(res$word))
+  res$word <- tolower(res$word)
   res
 }
 
-main <- function(corpus) {
-  adjectives <- get_adjectives(corpus, "jj")
-
+get_cooccurrence <- function(corpus, tag) {
+  adjectives <- get_pos(corpus, tag)
   counts <- table(adjectives$word)
   .pairs <- count_pairs_per_sentence(adjectives$word, adjectives$sentence)
 
+  # join counts with word and collocate
   cbind(.pairs,
     as.vector(counts[.pairs[, 1]]),
     as.vector(counts[.pairs[, 2]])
@@ -40,5 +40,5 @@ main <- function(corpus) {
 
 data(brown)
 
-adjective_cooccurrence <- main(brown)
+adjective_cooccurrence <- get_cooccurrence(brown, pos = "jj")
 usethis::use_data(adjective_cooccurrence, overwrite = TRUE)
