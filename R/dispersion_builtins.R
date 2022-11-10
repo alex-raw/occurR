26226114a55dc16513d2eb7e4493866a68330ef5
Alexander Rauhut <alexraw@mailbox.org>
2022-11-10 19:09:20 +0100

    feat(disp): replace collapse::fmin/max with faster fastmatch::ctapply

--- R/dispersion_builtins.R
+++ R/dispersion_builtins.R
@@ -47,13 +47,11 @@ builtin_disp <- function() {
 }
 
 max_min0 <- function(x, group, n, range) {
-  if (requireNamespace("collapse", quietly = TRUE)) {
-    maxs <- collapse::fmax.default(x, group, use.g.names = FALSE, na.rm = FALSE)
-    mins <- collapse::fmin.default(x, group, use.g.names = FALSE, na.rm = FALSE)
-  } else {
-    maxs <- tapply(x, group, max, na.rm = FALSE)
-    mins <- tapply(x, group, min, na.rm = FALSE)
-  }
+  .tapply <- if (requireNamespace("fastmatch", quietly = TRUE))
+    fastmatch::ctapply else tapply
+
+  maxs <- .tapply(x, group, max, na.rm = FALSE)
+  mins <- .tapply(x, group, min, na.rm = FALSE)
 
   non_zero <- range >= n
   maxs[non_zero] <- maxs[non_zero] - mins[non_zero]
